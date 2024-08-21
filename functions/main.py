45dd0f71bf392d927f6325197c96f32b0ff8732a
Ahd Ghazal <ahdgh02@gmail.com>
2024-08-21 14:53:34 +0300

    commit

--- functions/main.py
+++ functions/main.py
@@ -281,4 +281,5 @@ def submit():
     return jsonify(response)
 
 if __name__ == '__main__':
-    app.run(host='127.0.0.1', port=4000, debug=True)
+    app.run(host='localhost', port=8080, debug=True)
+
